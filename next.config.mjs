e5d2d2027afe468a5c933529d56d7e938643fd59
Anil Pal <anilcodes01@gmail.com>
2025-11-12 19:00:39 +0530

    mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your Next.js config options go here if you have any.
+  // For a basic setup, this can be empty.
+  experimental: {}, // You can remove the old turbopack setting
+};
+
+export default nextConfig;
